0e9bd49083d5d8850b8fb8db46bee2abf8a342ed
Roberto Langarica <roberto.langarica@gmail.com>
2021-04-28 16:50:36 -0500

    src/store/store.ts: Handling the activities module src/store/store.ts: Added a global init action that calls an individual init action on every store module.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,21 @@
 import { createStore } from 'vuex'
+import basicCollection, { IBasicCollection } from './basic_collection';
 import config, { IConfigState } from "./config/config";
+import activities from "./activities/activities";
 
 export interface IGlobalState {
     config: IConfigState;
+    activities: IBasicCollection;
 }
 
 const store = createStore<IGlobalState>({
     modules: {
-        config
+        config, activities, hairs:basicCollection('hairs')
     },
     actions:{
-        initFromPersistentData({ dispatch}){
+        init({ dispatch}){
             Object.keys((this as any)._modulesNamespaceMap).forEach(module=>{
-                dispatch(`${module}initFromPersistentData`)
+                dispatch(`${module}init`)
             })
         }
     }
