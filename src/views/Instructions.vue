2756aefd0149f2495264beb15593ca1979b42030
Roberto Langarica <roberto.langarica@gmail.com>
2021-04-28 17:11:03 -0500

    Adding a welcome and an general instructions view

---
+++ src/views/Instructions.vue
@@ -0,0 +1,22 @@
+<template>
+    <ion-page>
+        <ion-content>
+            <p>
+                Texto de instrucciones
+            </p>
+            <ion-button router-link="/excercises">Next</ion-button>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IonPage, IonContent, IonButton } from '@ionic/vue';
+
+export default defineComponent({
+    components:{ IonPage, IonContent, IonButton },
+    setup() {
+        return {}
+    },
+})
+</script>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,30 @@
+<template>
+    <ion-page>
+        <ion-content>
+            <h1>Hola</h1>
+            <h5>Elige un lenguaje</h5>
+            <label for="wx">
+                <input type="checkbox" name="wx"/>
+                Wixarika
+            </label>
+            <label for="sp">
+                <input type="checkbox" name="sp"/>
+                Español
+            </label>
+            <ion-button router-link="/instructions">Next</ion-button>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IonPage, IonContent, IonButton } from '@ionic/vue';
+
+export default defineComponent({
+    name:"welcome",
+    components:{ IonPage, IonContent, IonButton },
+    setup() {
+        return {}
+    },
+})
+</script>
