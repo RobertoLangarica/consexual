77896e141c351c94acff533c963dd788635670a0
Roberto Langarica <roberto.langarica@gmail.com>
2021-05-04 19:36:01 -0500

    Adding Images screen for testing firestore images

---
+++ src/views/Images.vue
@@ -0,0 +1,17 @@
+<template>
+    <ion-page>
+        <ion-content>
+            <remote-image src="Screen Shot 2021-04-28 at 22.18.30.png" style="width:78px; height:78px;"/>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IonContent, IonPage } from '@ionic/vue'
+import RemoteImage from '@/components/RemoteImage.vue'
+
+export default defineComponent({
+    components: { IonContent, IonPage, RemoteImage },
+})
+</script>
